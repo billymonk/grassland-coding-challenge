2552a7d2d75cf5fdce41f355b01c23f2d78d0ca3
Billy Monk <billy.monk@gmail.com>
2025-08-09 19:45:23 -0400

    Messaging Goal 1

--- src/app/messaging-challenge/components/messaging/messaging.component.ts
+++ src/app/messaging-challenge/components/messaging/messaging.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MessagingService } from '../../services/messaging.service';
+import { Observable } from 'rxjs';
+import { Message } from '../../models/message';
 
 @Component({
   selector: 'app-messaging',
@@ -6,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./messaging.component.scss']
 })
 export class MessagingComponent {
+  messages$: Observable<Message[]>;
 
+  constructor(private messagingService: MessagingService) {
+    this.messages$ = this.messagingService.messages$;
+  }
 }
