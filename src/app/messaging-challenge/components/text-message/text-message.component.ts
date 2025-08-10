994cdc3fca6dfadb45ab53f1ed5f7190a21c35f5
Billy Monk <billy.monk@gmail.com>
2025-08-09 22:41:08 -0400

    Messaging Goal 2

---
+++ src/app/messaging-challenge/components/text-message/text-message.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { BaseMessageComponent } from '../base-message/base-message.component';
+import { TextMessage } from '../../models/text-message';
+
+@Component({
+  selector: 'app-text-message',
+  templateUrl: './text-message.component.html',
+  styleUrls: ['./text-message.component.scss']
+})
+export class TextMessageComponent extends BaseMessageComponent {
+  override message: TextMessage;
+
+  constructor() {
+    super();
+  }
+
+  isFromAnna(): boolean {
+    return this.message.from === 'Anna';
+  }
+}
